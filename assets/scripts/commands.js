aa93af09fd9f8e08e18c92e1aef7c89642a6f3d6
laurpaik <laurpaik@gmail.com>
2017-07-31 19:48:48 -0400

    Implement Typed.js
    - It's easier than using the Codepen + flexbox
    - Will adjust to my box limitations
    - Overflow scroll for the win
    - Next steps: skip button and graph

--- assets/scripts/commands.js
+++ assets/scripts/commands.js
@@ -1,50 +1,49 @@
 'use strict';
+import Typed from 'typed.js';
 
 const content = document.querySelector('.input-box');
 
-const reset = function() {
+const resetForm = function() {
   content.value = '';
 };
 
-// need edge cases for misspellings, case errors, etc
-// const onStart = function (e) {
-//   e.preventDefault();
-//   console.log('START');
-//   if (content.value === 'start') {
-//     console.log('HEY');
-//     $('.results').text('hey');
-//     reset();
-//   }
-//   else if (content.value === 'why'){
-//     console.log('NO');
-//     $('.results').text('why');
-//   }
-// };
+const resetResults = function () {
+  $('.results').html('');
+};
 
+let startText = '<p>Data Science is super easy!';
+let mistakeText = '<p>Hmmm, that\'s not quite right. Check your spelling and caps!</p>';
+let lorem = '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
 
 const submit = function (e) {
   e.preventDefault();
-  console.log('hey');
+  resetResults();
   if (content.value === 'start') {
-    console.log('HEY');
-    $('.results').html('<p>omg you did it<span>|</span></p> </p>');
-    reset();
-  }
-  else if (content.value === 'why'){
-    console.log('NO');
-    $('.results').text('why');
+    let start = new Typed(".results", {
+        strings: [startText],
+        typeSpeed: 30,
+      });
+    resetForm();
+  } else if (content.value === 'why'){
+    let why = new Typed('.results', {
+      strings: [lorem],
+      typeSpeed: 30,
+    })
+  } else {
+    let mistake = new Typed('.results', {
+      strings: [mistakeText],
+      typeSpeed: 30,
+    });
   }
 };
 
 const addEventHandlers = () => {
   $('.user-form').on('submit', function () {
     submit(event);
-    // onStart(event);
     console.log(content.value, 'CONTENT');
   });
 };
 
 module.exports = {
-  // onStart,
   addEventHandlers
 };
